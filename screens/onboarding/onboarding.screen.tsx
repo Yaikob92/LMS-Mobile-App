8bf7c893d7d8316c71584b68cffe4051a408152b
Yaikob/92 <wyaikob@gmail.com>
2026-03-06 16:44:52 +0300

    Implement an onboarding slider with wave transition effect using new Slider, Wave, and Slide components.

--- screens/onboarding/onboarding.screen.tsx
+++ screens/onboarding/onboarding.screen.tsx
@@ -1,11 +1,25 @@
+import Slide from "@/components/onboarding/slide";
+import Slider from "@/components/onboarding/slider";
+import { onBoardingSlides } from "@/config/constants";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet } from "react-native";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
-export default function OnboardingScreen() {
+export default function Slider() {
   return (
-    <View>
-      <Text>OnboardingScreen</Text>
-    </View>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <Slider
+        key={index}
+        index={index}
+        setindex={setIndex}
+        prev={
+          prev && <Slide slide={prev} totalSlidess={onBoardingSlides.length} />
+        }
+        next={
+          next && <Slide slide={next} totalSlidess={onBoardingSlides.length} />
+        }
+      ></Slider>
+    </GestureHandlerRootView>
   );
 }
 
